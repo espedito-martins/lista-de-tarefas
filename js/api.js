0163736af2f95cf427366be0a14e85885a403809
Espedito Martins <espedito.martins.dev@gmail.com>
2026-01-06 22:55:52 -0300

    feat(api): add buscarTarefas (GET /tarefas)

---
+++ js/api.js
@@ -0,0 +1,24 @@
+const URL_BASE = 'http://localhost:3000'
+
+
+const api = {
+
+    async buscartarefas(){
+
+        try {
+            const tarefas = await axios.get(`${URL_BASE}/tarefas`)
+            return tarefas.data.map(tarefas => {
+                return {
+                    ...tarefas,
+                    criadoEm: new Date(tarefas.data),
+                    atualizadoEm: new Date(tarefas.data),
+                    concluidoEm: new Date(tarefas.data)
+                }
+            })
+        } catch (error) {
+            return alert('Erro ao carregar tarefas!')
+        }
+
+    }
+
+}
