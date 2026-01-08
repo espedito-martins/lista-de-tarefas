a6ed6ede33a6f860aef5fccfc1d86a99213ceae4
Espedito Martins <espedito.martins.dev@gmail.com>
2026-01-08 13:16:01 -0300

    feat: cadastrar tarefa (POST) via formulário, main primeira parte da main concluida

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 import ui from "./ui.js";
 import api from "./api.js";
 
+console.log("main.js carregou")
+
+const formulario = document.querySelector('#task-form')
+
 ui.renderizarTarefas()
 
+formulario.addEventListener('submit', async (evento) => {
+
+    evento.preventDefault()
+
+    const titulo = document.querySelector('#title').value
+    const descricao = document.querySelector('#description').value
+    const prioridade = document.querySelector('#priority').value
+    const status = document.querySelector('#status').value
+
+    await api.cadastrarTarefa({titulo, descricao, prioridade, status})
+
+    await ui.renderizarTarefas()
+
+})
